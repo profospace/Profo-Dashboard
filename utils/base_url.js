eade22c61f748e9062d9cfb8147787cafc514022
Anurag <anuragsonkar053@gmail.com>
2025-03-10 16:32:05 +0530

    base_url_change

--- utils/base_url.js
+++ utils/base_url.js
@@ -1,7 +1,7 @@
 // Propertify
-// export const base_url = 'https://propertify.onrender.com'
-// export const project_upload_url = 'https://propertify.onrender.com'
+export const base_url = 'https://propertify.onrender.com'
+export const project_upload_url = 'https://propertify.onrender.com'
 
 // Local
-export const base_url = 'http://localhost:5053'
-export const project_upload_url = 'http://localhost:5053'
+// export const base_url = 'http://localhost:5053'
+// export const project_upload_url = 'http://localhost:5053'
